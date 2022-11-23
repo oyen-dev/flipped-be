b9c8a581c041e3b2f77f5d9f9b75517c5b0ff3fc
Fikri Mustofa <fikrimustofa024@gmail.com>
2022-11-23 02:29:42 +0000

    feat(classBinder): bind all functions in a class

--- src/utils/classBinder.js
+++ src/utils/classBinder.js
@@ -2,6 +2,14 @@ const getAllFunctions = (object) => {
   return Object.getOwnPropertyNames(Object.getPrototypeOf(object))
 }
 
+const bindAll = (self) => {
+  for (const func of getAllFunctions(self)) {
+    if (func === 'constructor') continue
+    self[func] = self[func].bind(self)
+  }
+}
+
 module.exports = {
-  getAllFunctions
+  getAllFunctions,
+  bindAll
 }
