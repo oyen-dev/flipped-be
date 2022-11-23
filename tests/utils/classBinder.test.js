f8a90419b17d37e10a640f68d1df49b93045a793
Fikri Mustofa <fikrimustofa024@gmail.com>
2022-11-23 01:49:08 +0000

    test: add test getAllFunctions in ClassBinder

--- tests/utils/classBinder.test.js
+++ tests/utils/classBinder.test.js
@@ -11,10 +11,27 @@ describe('ClassBinder', () => {
     }
   }
 
+  class AnotherClass {
+    constructor () {
+      this.rateCount = 500
+    }
+
+    fetchCounter () {
+      return this.rateCount
+    }
+  }
+
   describe('getAllFunctions', () => {
     it('returns all functions name in a class', () => {
-      const functions = getAllFunctions(SampleClass)
+      const sampleObj = new SampleClass()
+      const functions = getAllFunctions(sampleObj)
       expect(functions).toEqual(['constructor', 'getCounter'])
+    })
+
+    it('returns all functions name in another class', () => {
+      const anotherObj = new AnotherClass()
+      const functions = getAllFunctions(anotherObj)
+      expect(functions).toEqual(['constructor', 'fetchCounter'])
     })
   })
 })
