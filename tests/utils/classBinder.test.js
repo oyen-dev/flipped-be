e97c8cc1504b4e0e3203883528d37ea6a29c8b92
Fikri Mustofa <fikrimustofa024@gmail.com>
2022-11-23 02:29:11 +0000

    test: create test bindAll in classBInder

--- tests/utils/classBinder.test.js
+++ tests/utils/classBinder.test.js
@@ -1,27 +1,27 @@
-const { getAllFunctions } = require('../../src/utils/classBinder')
+const { getAllFunctions, bindAll } = require('../../src/utils/classBinder')
 
 describe('ClassBinder', () => {
-  class SampleClass {
-    constructor () {
-      this.rateCount = 200
-    }
-
-    getCounter () {
-      return this.rateCount
-    }
-  }
-
-  class AnotherClass {
-    constructor () {
-      this.rateCount = 500
-    }
-
-    fetchCounter () {
-      return this.rateCount
-    }
-  }
-
   describe('getAllFunctions', () => {
+    class SampleClass {
+      constructor () {
+        this.rateCount = 200
+      }
+
+      getCounter () {
+        return this.rateCount
+      }
+    }
+
+    class AnotherClass {
+      constructor () {
+        this.rateCount = 500
+      }
+
+      fetchCounter () {
+        return this.rateCount
+      }
+    }
+
     it('returns all functions name in a class', () => {
       const sampleObj = new SampleClass()
       const functions = getAllFunctions(sampleObj)
@@ -32,6 +32,40 @@ describe('ClassBinder', () => {
       const anotherObj = new AnotherClass()
       const functions = getAllFunctions(anotherObj)
       expect(functions).toEqual(['constructor', 'fetchCounter'])
+    })
+  })
+
+  describe('bindAll', () => {
+    class SampleBindedClass {
+      constructor () {
+        this.rateCount = 200
+        bindAll(this)
+      }
+
+      getCounter () {
+        return this.rateCount
+      }
+    }
+
+    class AnotherClass {
+      constructor () {
+        this.rateCount = 500
+        bindAll(this)
+      }
+
+      fetchCounter () {
+        return this.rateCount
+      }
+    }
+
+    it('returns an object with binded functions', async () => {
+      const sampleObj = new SampleBindedClass()
+      expect(await sampleObj.getCounter()).toEqual(200)
+    })
+
+    it('returns another object with binded functions', async () => {
+      const anotherObj = new AnotherClass()
+      expect(await anotherObj.fetchCounter()).toEqual(500)
     })
   })
 })
