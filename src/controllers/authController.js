0fee4932d7eb967d080604ddc365fd6982ce7eed
Fikri Mustofa <fikrimustofa024@gmail.com>
2022-11-23 23:34:28 +0000

    refactor: remove try catch block in each functions on AuthController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -18,150 +18,129 @@ class AuthController {
     const payload = req.body
     const { email, password } = payload
 
-    try {
-      // Validate payload
-      this._validator.validateRegister(payload)
+    // Validate payload
+    this._validator.validateRegister(payload)
 
-      // Check if user already exist
-      let user = await this._userService.findUserByEmail(email)
-      if (user) throw new ClientError('Sorry, this email was registered.', 400)
+    // Check if user already exist
+    let user = await this._userService.findUserByEmail(email)
+    if (user) throw new ClientError('Sorry, this email was registered.', 400)
 
-      // Hash password
-      const hashedPassword = await this._hashPassword.hash(password)
+    // Hash password
+    const hashedPassword = await this._hashPassword.hash(password)
 
-      // Modify payload
-      payload.password = hashedPassword
-      payload.dateOfBirth = new Date(payload.dateOfBirth)
+    // Modify payload
+    payload.password = hashedPassword
+    payload.dateOfBirth = new Date(payload.dateOfBirth)
 
-      // Create user
-      user = await this._userService.createUser(payload)
+    // Create user
+    user = await this._userService.createUser(payload)
 
-      // Generate token
-      const tokenDetails = await this._authService.createToken(user)
-      const { token } = tokenDetails
+    // Generate token
+    const tokenDetails = await this._authService.createToken(user)
+    const { token } = tokenDetails
 
-      // Send email
-      const url = process.env.CLIENT_URL || 'http://localhost:3000'
-      const message = {
-        name: user.fullName,
-        email,
-        link: `${url}/auth/verify?token=${token}`
-      }
-      await this._mailService.sendEmail(message, 'Hooray, Your Registration Success', 'register')
-
-      // Response
-      const response = this._response.success(201, 'Register success, please check your email to activate your account!')
-
-      return res.status(response.statusCode || 200).json(response)
-    } catch (error) {
-      // To do logger error
-      // console.log(error)
-      return this._response.error(res, error)
+    // Send email
+    const url = process.env.CLIENT_URL || 'http://localhost:3000'
+    const message = {
+      name: user.fullName,
+      email,
+      link: `${url}/auth/verify?token=${token}`
     }
+    await this._mailService.sendEmail(message, 'Hooray, Your Registration Success', 'register')
+
+    // Response
+    const response = this._response.success(201, 'Register success, please check your email to activate your account!')
+
+    return res.status(response.statusCode || 200).json(response)
   }
 
   async login (req, res) {
     const payload = req.body
     const { email, password, remember } = payload
 
-    try {
-      // Validate payload
-      this._validator.validateLogin(payload)
+    // Validate payload
+    this._validator.validateLogin(payload)
 
-      // Find user
-      const user = await this._userService.findUserByEmail(email)
-      if (!user) throw new ClientError('You have entered an invalid username or password.', 400)
+    // Find user
+    const user = await this._userService.findUserByEmail(email)
+    if (!user) throw new ClientError('You have entered an invalid username or password.', 400)
 
-      // Check if account is actived
-      if (!user.isActivated) throw new ClientError('Sorry, this account is not actived yet. Please check your email to activate.', 400)
+    // Check if account is actived
+    if (!user.isActivated) throw new ClientError('Sorry, this account is not actived yet. Please check your email to activate.', 400)
 
-      // Check if account is deleted
-      if (user.isDeleted) throw new ClientError('Sorry, this account is deleted.', 400)
+    // Check if account is deleted
+    if (user.isDeleted) throw new ClientError('Sorry, this account is deleted.', 400)
 
-      // Check password
-      const isValidPassword = await this._hashPassword.compare(password, user.password)
-      if (!isValidPassword) throw new ClientError('You have entered an invalid username or password.', 400)
+    // Check password
+    const isValidPassword = await this._hashPassword.compare(password, user.password)
+    if (!isValidPassword) throw new ClientError('You have entered an invalid username or password.', 400)
 
-      // Generate token
-      const accessToken = await this._tokenize.sign(user, remember)
+    // Generate token
+    const accessToken = await this._tokenize.sign(user, remember)
 
-      // Response
-      const response = this._response.success(200, 'Login success!', { accessToken, expiredIn: remember ? '7d' : '1d' })
+    // Response
+    const response = this._response.success(200, 'Login success!', { accessToken, expiredIn: remember ? '7d' : '1d' })
 
-      return res.status(response.statusCode || 200).json(response)
-    } catch (error) {
-      // To do logger error
-      console.log(error)
-      return this._response.error(res, error)
-    }
+    return res.status(response.statusCode || 200).json(response)
   }
 
   async getAuthProfile (req, res) {
     const token = req.headers.authorization
-    try {
-      // Check token is exist
-      if (!token) throw new ClientError('Unauthorized', 401)
 
-      // Validate token
-      const { _id } = await this._tokenize.verify(token)
+    // Check token is exist
+    if (!token) throw new ClientError('Unauthorized', 401)
 
-      // Find user
-      const user = await this._userService.findUserById(_id)
-      if (!user) throw new ClientError('Unauthorized', 401)
+    // Validate token
+    const { _id } = await this._tokenize.verify(token)
 
-      const details = {
-        _id: user._id,
-        email: user.email,
-        name: user.fullName,
-        picture: user.picture,
-        role: user.role
-      }
+    // Find user
+    const user = await this._userService.findUserById(_id)
+    if (!user) throw new ClientError('Unauthorized', 401)
 
-      // Response
-      const response = this._response.success(200, 'Get profile success!', details)
-
-      return res.status(response.statusCode || 200).json(response)
-    } catch (error) {
-      // To do logger error
-      return this._response.error(res, error)
+    const details = {
+      _id: user._id,
+      email: user.email,
+      name: user.fullName,
+      picture: user.picture,
+      role: user.role
     }
+
+    // Response
+    const response = this._response.success(200, 'Get profile success!', details)
+
+    return res.status(response.statusCode || 200).json(response)
   }
 
   async forgotPassword (req, res) {
     const payload = req.body
     const { email } = payload
 
-    try {
-      // Validate payload
-      this._validator.validateForgotPassword(payload)
+    // Validate payload
+    this._validator.validateForgotPassword(payload)
 
-      // Find user
-      const user = await this._userService.findUserByEmail(email)
-      if (!user) throw new ClientError('Sorry, this email is not registered.', 400)
+    // Find user
+    const user = await this._userService.findUserByEmail(email)
+    if (!user) throw new ClientError('Sorry, this email is not registered.', 400)
 
-      // Check if account is deleted
-      if (user.isDeleted) throw new ClientError('Sorry, this account is deleted.', 400)
+    // Check if account is deleted
+    if (user.isDeleted) throw new ClientError('Sorry, this account is deleted.', 400)
 
-      // Generate token
-      const tokenDetails = await this._authService.createToken(user)
-      const { token } = tokenDetails
+    // Generate token
+    const tokenDetails = await this._authService.createToken(user)
+    const { token } = tokenDetails
 
-      // Send email
-      const url = process.env.CLIENT_URL || 'http://localhost:3000'
-      const message = {
-        name: user.fullName,
-        email,
-        link: `${url}/auth/reset-password?token=${token}`
-      }
-      await this._mailService.sendEmail(message, 'Request Reset Password', 'forgot')
-
-      // Response
-      const response = this._response.success(200, 'We have sent you an email to reset your password!')
-      return res.status(response.statusCode || 200).json(response)
-    } catch (error) {
-      // To do logger error
-      return this._response.error(res, error)
+    // Send email
+    const url = process.env.CLIENT_URL || 'http://localhost:3000'
+    const message = {
+      name: user.fullName,
+      email,
+      link: `${url}/auth/reset-password?token=${token}`
     }
+    await this._mailService.sendEmail(message, 'Request Reset Password', 'forgot')
+
+    // Response
+    const response = this._response.success(200, 'We have sent you an email to reset your password!')
+    return res.status(response.statusCode || 200).json(response)
   }
 
   async resetPassword (req, res) {
@@ -169,106 +148,91 @@ class AuthController {
     const payload = req.body
     const { password } = payload
 
-    try {
-      // Check token is exist
-      if (!token) throw new ClientError('Unauthorized', 401)
+    // Check token is exist
+    if (!token) throw new ClientError('Unauthorized', 401)
 
-      // Validate token
-      this._validator.validateCheckToken({ token })
+    // Validate token
+    this._validator.validateCheckToken({ token })
 
-      // Validate payload
-      this._validator.validateResetPassword(payload)
+    // Validate payload
+    this._validator.validateResetPassword(payload)
 
-      // Find token
-      const tokenDetails = await this._authService.findTokenByToken(token)
-      if (!tokenDetails) throw new ClientError('Unauthorized', 401)
+    // Find token
+    const tokenDetails = await this._authService.findTokenByToken(token)
+    if (!tokenDetails) throw new ClientError('Unauthorized', 401)
 
-      // Get user based on token
-      const { email } = tokenDetails
-      const user = await this._userService.findUserByEmail(email)
+    // Get user based on token
+    const { email } = tokenDetails
+    const user = await this._userService.findUserByEmail(email)
 
-      // Check if account is actived
-      if (!user.isActivated) throw new ClientError('Sorry, this account is not actived yet.', 400)
+    // Check if account is actived
+    if (!user.isActivated) throw new ClientError('Sorry, this account is not actived yet.', 400)
 
-      // Hash password
-      const hashedPassword = await this._hashPassword.hash(password)
+    // Hash password
+    const hashedPassword = await this._hashPassword.hash(password)
 
-      // Update user
-      user.password = hashedPassword
-      user.updatedAt = new Date().toISOString()
-      await user.save()
+    // Update user
+    user.password = hashedPassword
+    user.updatedAt = new Date().toISOString()
+    await user.save()
 
-      // Delete token
-      await this._authService.deleteToken(email)
+    // Delete token
+    await this._authService.deleteToken(email)
 
-      // Response
-      const response = this._response.success(200, 'Reset password success.')
+    // Response
+    const response = this._response.success(200, 'Reset password success.')
 
-      return res.status(response.statusCode || 200).json(response)
-    } catch (error) {
-      // To do logger error
-      return this._response.error(res, error)
-    }
+    return res.status(response.statusCode || 200).json(response)
   }
 
   async checkToken (req, res) {
     const token = req.query.token
 
-    try {
-      // Check token is exist
-      if (!token) throw new ClientError('Unauthorized', 401)
+    // Check token is exist
+    if (!token) throw new ClientError('Unauthorized', 401)
 
-      // Validate token
-      this._validator.validateCheckToken({ token })
+    // Validate token
+    this._validator.validateCheckToken({ token })
 
-      // Find token
-      const tokenDetails = await this._authService.findTokenByToken(token)
-      if (!tokenDetails) throw new ClientError('Invalid reset password token', 401)
+    // Find token
+    const tokenDetails = await this._authService.findTokenByToken(token)
+    if (!tokenDetails) throw new ClientError('Invalid reset password token', 401)
 
-      // Response
-      const response = this._response.success(200, 'Token is valid.')
+    // Response
+    const response = this._response.success(200, 'Token is valid.')
 
-      return res.status(response.statusCode || 200).json(response)
-    } catch (error) {
-      // To do logger error
-      return this._response.error(res, error)
-    }
+    return res.status(response.statusCode || 200).json(response)
   }
 
   async verifyAccount (req, res) {
     const payload = req.query
     const { token } = payload
 
-    try {
-      // Validate payload
-      this._validator.validateCheckToken(payload)
+    // Validate payload
+    this._validator.validateCheckToken(payload)
 
-      // Find token
-      const tokenDetails = await this._authService.findTokenByToken(token)
-      if (!tokenDetails) throw new ClientError('Invalid activation token', 401)
+    // Find token
+    const tokenDetails = await this._authService.findTokenByToken(token)
+    if (!tokenDetails) throw new ClientError('Invalid activation token', 401)
 
-      // Get user based on token
-      const { email } = tokenDetails
-      const user = await this._userService.findUserByEmail(email)
-      if (!user) throw new ClientError('Unauthorized', 401)
+    // Get user based on token
+    const { email } = tokenDetails
+    const user = await this._userService.findUserByEmail(email)
+    if (!user) throw new ClientError('Unauthorized', 401)
 
-      // Activate user
-      user.isActivated = true
-      user.verifiedAt = new Date()
-      user.updatedAt = new Date()
-      await user.save()
+    // Activate user
+    user.isActivated = true
+    user.verifiedAt = new Date()
+    user.updatedAt = new Date()
+    await user.save()
 
-      // Delete token
-      await this._authService.deleteToken(email)
+    // Delete token
+    await this._authService.deleteToken(email)
 
-      // Response
-      const response = this._response.success(200, 'Congratulations! Your account has been verified.')
+    // Response
+    const response = this._response.success(200, 'Congratulations! Your account has been verified.')
 
-      return res.status(response.statusCode || 200).json(response)
-    } catch (error) {
-      // To do logger error
-      return this._response.error(res, error)
-    }
+    return res.status(response.statusCode || 200).json(response)
   }
 }
 
