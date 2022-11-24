8d71efced43043849b86a80a51afbd6f3ae154e7
Fikri Mustofa <fikrimustofa024@gmail.com>
2022-11-24 10:24:01 +0000

    feat: apply checking valid class when getting presences

--- src/controllers/presenceController.js
+++ src/controllers/presenceController.js
@@ -1,9 +1,15 @@
+const { bindAll } = require('../utils/classBinder')
+
 class PresenceController {
   constructor (presenceService, classService) {
     this.presenceService = presenceService
+    this.classService = classService
+
+    bindAll(this)
   }
 
-  getPresences (req, res) {
+  async getPresences (req, res) {
+    const classroom = await this.classService.getClass(req.params.classid)
     res.send('')
   }
 }
