28b1a5bcace1a45092b375dd10a5103ff8531229
Fikri Mustofa <fikrimustofa024@gmail.com>
2022-11-24 11:06:02 +0000

    fix: add mergeParams option in presenceRoutes

--- src/routes/presenceRoutes.js
+++ src/routes/presenceRoutes.js
@@ -2,7 +2,7 @@ const { MyRouter } = require('../myserver')
 
 class PresenceRoutes {
   constructor (verifyToken, needRoles, presenceController) {
-    const router = MyRouter()
+    const router = MyRouter({ mergeParams: true })
     router.get('/', verifyToken, needRoles(['ADMIN', 'TEACHER']), presenceController.getPresences)
     this.router = router
   }
