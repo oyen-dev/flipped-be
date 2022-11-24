e95fe9c8983b292e3cf60dd8c27372d08abc7e0e
Fikri Mustofa <fikrimustofa024@gmail.com>
2022-11-24 09:15:49 +0000

    fix: change presenceRoutes to use MyRouter

--- src/routes/presenceRoutes.js
+++ src/routes/presenceRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const { MyRouter } = require('../myserver')
 
 class PresenceRoutes {
   constructor (verifyToken, needRoles, presenceController) {
-    const router = express.Router()
+    const router = MyRouter()
     router.get('/', verifyToken, needRoles(['ADMIN', 'TEACHER']), presenceController.getPresences)
     this.router = router
   }
